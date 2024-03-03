221ace88c06c985b93f3313f830d24a1d6d764a9
Чупинин Антон <chupinin@intervolga.ru>
2024-03-03 21:29:07 +0300

    added function checkArgs and makeOperation

--- src/main/java/json/handler/UpsStatusUtils.java
+++ src/main/java/json/handler/UpsStatusUtils.java
@@ -7,6 +7,10 @@ import java.util.stream.Collectors;
 
 public class UpsStatusUtils {
 
+    public static final String AVG = "avg";
+    public static final String MAX = "max";
+    public static final String VALUES = "values";
+
     public static double avg(List<UpsStatus> upsStatuses){
         List<Integer> batteryRunTimeRemaingList = upsStatuses.stream().filter(n -> Objects.nonNull(n.getBatteryRunTimeRemaining())).map(n -> n.getBatteryRunTimeRemaining()).collect(Collectors.toList());
         long sum = batteryRunTimeRemaingList.stream().mapToLong(n -> n).sum();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,72 @@ import json.handler.UpsStatusParser;
 import json.handler.UpsStatusUtils;
 
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        String fileName = "E:\\JsonHandlerProject\\src\\test\\example\\testData.json";
-        List<UpsStatus> list = new UpsStatusParser().parse(fileName);
-        System.out.println(list.size());
-        System.out.println("avg =" + UpsStatusUtils.avg(list));
-        System.out.println("max = " +UpsStatusUtils.max(list));
-        System.out.println("values = " + UpsStatusUtils.values(list));
+//        String fileName = "E:\\JsonHandlerProject\\src\\test\\example\\testData.json";
+//        List<UpsStatus> list = new UpsStatusParser().parse(fileName);
+//        System.out.println(list.size());
+//        System.out.println("avg =" + UpsStatusUtils.avg(list));
+//        System.out.println("max = " +UpsStatusUtils.max(list));
+//        System.out.println("values = " + UpsStatusUtils.values(list));
+
+        try {
+            checkArgs(args);
+            List<UpsStatus> list = new UpsStatusParser().parse(args[1]);
+            makeOperation(args[0], list);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static void checkArgs(String[] args) {
+
+        if (args.length != 2) {
+            throw new RuntimeException("program should have 2 arguments: <functionName> <fileName>");
+        }
+
+        boolean isExistsFuntions = true;
+
+        if (UpsStatusUtils.VALUES.equals(args[0])) {
+            isExistsFuntions = false;
+        }
+
+        if (UpsStatusUtils.MAX.equals(args[0])) {
+            isExistsFuntions = false;
+        }
+
+        if (UpsStatusUtils.AVG.equals(args[0])) {
+            isExistsFuntions = false;
+        }
+
+        if (!isExistsFuntions) {
+            throw new IllegalArgumentException("no function with name \"" + args[0] + "\"");
+        }
+    }
+
+    public static void makeOperation(String functionName, List<UpsStatus> upsStatuses) {
+
+        String result = "result: ";
+        switch (functionName){
+            case UpsStatusUtils.AVG:
+                double avg = UpsStatusUtils.avg(upsStatuses);
+                result += avg;
+                break;
+            case UpsStatusUtils.MAX:
+                int max = UpsStatusUtils.max(upsStatuses);
+                result += max;
+                break;
+            case UpsStatusUtils.VALUES:
+                Set<String> values = UpsStatusUtils.values(upsStatuses);
+                result += values;
+                break;
+        }
+
+        System.out.println(result);
     }
 }
 
